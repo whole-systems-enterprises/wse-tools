0b628943201f7e6fa2853f2cdb41a6c89771b70b
whole-systems-enterprises <whole.systems.enterprises@gmail.com>
2019-02-18 15:06:12 -0800

    Added loss and metric plot.

--- machine_learning/display.py
+++ machine_learning/display.py
@@ -27,3 +27,39 @@ def make_roc_curve(y_known, y_score, figsize=None, title='ROC Curve', filename=N
     plt.close()
 
     return roc_auc
+
+
+#
+# plot loss and metric
+#
+def plot_loss_and_metric(metric, history, loss_and_metric_filename):
+
+    history_dict = history.history
+    
+    loss_values = history_dict['loss']
+    val_loss_values = history_dict['val_loss']
+    metric_values = history_dict[metric]
+    val_metric_values = history_dict['val_' + metric]
+
+    epochs = range(1, len(loss_values) + 1)
+
+    plt.figure(figsize=[10, 12])
+
+    plt.subplot(2, 1, 1)
+    plt.plot(epochs, loss_values, 'bo', label='Training Loss')
+    plt.plot(epochs, val_loss_values, 'b', label='Validation Loss')
+    plt.title('Training and Validation Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+
+    plt.subplot(2, 1, 2)
+    plt.plot(epochs, metric_values, 'bo', label='Training Metric')
+    plt.plot(epochs, val_metric_values, 'b', label='Validation Metric')
+    plt.title('Training and Validation Metric')
+    plt.xlabel('Epochs')
+    plt.ylabel('Metric')
+    plt.legend()
+
+    plt.savefig(loss_and_metric_filename)
+    plt.close()
