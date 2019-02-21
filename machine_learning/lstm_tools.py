c3c1c9f6df597c06f16d8d27e9a1451790243daf
Emily Marie Williams <emily.marie.williams@protonmail.com>
2019-02-21 11:42:59 -0800

    Added a function for cases with word-embedded text input.

--- machine_learning/lstm_tools.py
+++ machine_learning/lstm_tools.py
@@ -5,6 +5,7 @@
 from keras import layers
 from keras.models import Sequential
 from keras import regularizers
+from keras.models import Model
 
 import uuid
 
@@ -53,7 +54,7 @@ def compile_generic_LSTM_classifier(model, properties, optimizer='rmsprop', loss
 #
 # fit the generic Keras LSTM classifer given above
 #
-def fit_generic_LSTM_classifier(model, train_x, train_y, val_x, val_y, epochs=20, batch_size=32):
+def fit_generic_LSTM_classifier(model, properties, train_x, train_y, val_x, val_y, epochs=20, batch_size=32):
     history = model.fit(
         train_x,
         train_y,
@@ -68,3 +69,35 @@ def fit_generic_LSTM_classifier(model, train_x, train_y, val_x, val_y, epochs=20
         }
 
     return history, properties
+
+
+#
+# create LSTM model framework
+#
+# This function is heavily modified from that found in https://github.com/natel9178/CS230-news-bias/blob/master/train.py
+#
+def create_LSTM_for_word_embedded(number_of_layers, number_of_cells_per_layer, embedding_layer, maximum_sequence_length, l2_regularization_constant = 0.001, dropout_rate = 0.2):
+
+    sequence_input = layers.Input(shape=(maximum_sequence_length,), dtype='int32')
+    embedded_sequences = embedding_layer(sequence_input)
+
+    X = embedded_sequences
+    for i in range(number_of_layers - 1):
+        X = layers.LSTM(number_of_cells_per_layer, kernel_regularizer=regularizers.l2(l2_regularization_constant), return_sequences=True)(X)
+        X = layers.Dropout(dropout_rate)(X)
+    X = layers.LSTM(number_of_cells_per_layer, kernel_regularizer=regularizers.l2(l2_regularization_constant), return_sequences=False)(X)
+    X = layers.Dropout(dropout_rate)(X)
+    X = layers.Dense(1)(X)
+    preds = layers.Activation('sigmoid')(X)
+
+    properties = {
+        'model ID' : str(uuid.uuid4()),
+        'construction' : {
+            'number_of_layers' : number_of_layers,
+            'number_of_cells_per_layer' : number_of_cells_per_layer,
+            'l2_regularization_constant' :  l2_regularization_constant,
+            'dropout_rate' : dropout_rate,
+            },
+        }
+    
+    return Model(sequence_input, preds), properties
