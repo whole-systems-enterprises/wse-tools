1f244d1999f3bd24c5bde965a07b33af921aa5ea
whole-systems-enterprises <whole.systems.enterprises@gmail.com>
2019-05-03 21:32:17 -0700

    Added commentary.

--- statistics/bootstrapping.py
+++ statistics/bootstrapping.py
@@ -11,14 +11,22 @@ from numpy.random import choice
 # The "function" argument is something like np.mean, np.median, or np.std.
 #
 def bootstrap_1D_no_for_loops(x, function, repetitions, number_to_sample_with_replacement = None, confidence_interval_as_proportion = 0.95):
+
+    # decide whether each resampled set should be as large as the original sample set
     if number_to_sample_with_replacement == None:
         number_to_sample_with_replacement = len(x)
+
+    # draw n = repetitions number of bootstrap samples 
     bootstrap_samples = choice(x, size = (number_to_sample_with_replacement, repetitions))
+
+    # apply the function to each repetition and sort the resulting 1D array
     function_applied_to_each_bootstrap_sample = function(bootstrap_samples, axis=0)
     function_applied_to_each_bootstrap_sample.sort()
 
+    # compute confidence interval
     lower_bounds = (100. - (100. * confidence_interval_as_proportion)) / 2.
     upper_bounds = 100. - lower_bounds
     confidence_interval = percentile(function_applied_to_each_bootstrap_sample, [lower_bounds, upper_bounds])
 
+    # return results
     return function_applied_to_each_bootstrap_sample, confidence_interval
